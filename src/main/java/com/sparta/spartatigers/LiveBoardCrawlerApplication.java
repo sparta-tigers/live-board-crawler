74480ad35ac0ccd201f8dcbd90df385acea16806
jungmin <jungmini0601@gmail.com>
2025-08-18 17:05:37 +0900

    Initial Commit

---
+++ src/main/java/com/sparta/spartatigers/LiveBoardCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartatigers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBoardCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveBoardCrawlerApplication.class, args);
+    }
+
+}
